029e0479bbea819399732f65b016baf6d04578b1
OlenaZorina <zorinae83@gmail.com>
2023-08-24 12:59:09 +0300

    Add file 1

--- Test_1.py
+++ Test_1.py
@@ -15,8 +15,8 @@ time.sleep(10)
 
 #browser.element('#todo-list>li:nth-of-type(2) .toggle').click()
 # browser.element('#ember9').click()
-#browser.element(by.text('Completed')).click()
-#browser.all('#todo-list>li.completed').should(have.texts('b'))
+browser.element(by.text('Completed')).click()
+browser.all('#todo-list>li.completed').should(have.texts('b'))
 #
 # browser.element(by.text('Active')).click()
 # browser.all('#filters>li.active').should(have.exact_texts('a', 'c'))
