c58ac5cd824071459cb206ca5d79e33f1aa1b5f2
OlenaZorina <zorinae83@gmail.com>
2023-08-24 13:11:47 +0300

    DZ 1

--- Test_2.py
+++ Test_2.py
@@ -8,11 +8,8 @@ browser.element('#new-todo').type('b').press_enter()
 browser.element('#new-todo').type('c').press_enter()
 browser.all('#todo-list>li').should(have.exact_texts('a', 'b', 'c'))
 #
-browser.element('#todo-list>li:nth-of-type(2) .toggle').click()
-browser.element(by.text('Completed')).click()
-browser.all('#todo-list>li.completed').should(have.texts('b'))
-#
-# browser.element(by.text('Active')).click()
-# browser.all('#filters>li.active').should(have.exact_texts('a', 'c'))
-# time.sleep(60)
-# browser.element('#ember9').click() - не вірно
+browser.element('#toggle-all').click()
+browser.element(by.text('Clear completed')).click()
+browser.all('#todo-list>li').should(have.exact_texts())
+time.sleep(10)
+
